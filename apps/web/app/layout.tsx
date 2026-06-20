b3bcf469ccb71f948c99dcf35df65416a1f4643f
CaiorFeliciano <caiorfeliciano@gmail.com>
2026-06-20 19:11:08 -0300

    chore: setup shadcn/ui

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,6 +5,11 @@ export const metadata: Metadata = {};
 
 import { AuthProvider } from "@/contexts/AuthContext";
 import "./globals.css";
+import { JetBrains_Mono } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
+
 
 export default function RootLayout({
   children,
@@ -12,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={cn("font-mono", jetbrainsMono.variable)}>
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
